16e1ca2abddc986c07a19235c8f6a50245fe40b1
thiaguera00 <thiagocaetano155@gmail.com>
2024-10-08 16:58:05 -0300

    feat: added api integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './Home/App'
+// import App from './Home/App'
 import './index.css'
+import GerarQuestao from './Questoes'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <GerarQuestao />
   </StrictMode>,
 )
