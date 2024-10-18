fdebeed55aefd1be632d293a93caf577e3aee23a
thiaguera00 <thiagocaetano155@gmail.com>
2024-10-18 19:51:37 -0300

    feat: integrado registro com backend

--- src/services/iaService.ts
+++ src/services/iaService.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-
-const API_URL = 'http://localhost:3000';
+const API_URL = process.env.API_URL;
 
 const gerarQuestaoIa = async (level: string, content: string) => {
   try {
