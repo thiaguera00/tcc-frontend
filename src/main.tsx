0d054cd1c3340e448de1ba271794b750ee9d8e92
thiaguera00 <thiagocaetano155@gmail.com>
2024-10-27 14:49:01 -0300

    feat: adicionado rota para pagina de conquista

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,7 @@ import { Perfil } from './pages/perfil';
 import { Pesquisa } from './pages/pesquisa';
 import { PerfilEditar } from './pages/PerfilEditar/editarPerfil';
 import { Atividades } from './pages/atividades';
+import { TelaConquista } from './pages/conquistas/conquistas';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -22,6 +23,7 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/perfil" element={<Perfil />} />
         <Route path="/perfilEditar" element={<PerfilEditar />} />
         <Route path="/atividades" element={<Atividades />} />
+        <Route path="/telaConquista" element={<TelaConquista />} />
 
         <Route
           path="/paginaInicial"
